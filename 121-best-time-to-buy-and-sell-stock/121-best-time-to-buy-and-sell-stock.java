9603b433fefdc4d4280d06bcea54a72c2e8fdc82
Simran Bhatt <35162886+simranbhatt@users.noreply.github.com>
2022-09-09 22:25:58 +0530

    Time: 3 ms (69.45%), Space: 83.2 MB (66.08%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -4,10 +4,14 @@ class Solution {
         int currentProf = 0;
         int maxProf = 0;
         for(int price : prices) {
+            //is today's price less than the minimum price we've seen so far?
             if(price < min) {
                 min = price;
             }
+            //what is the profit if we sell at today's price assuming we bought at min?
             currentProf = price - min;
+            //
+            //if current profit is higher, mark that as the max profit
             if(currentProf > maxProf) {
                 maxProf = currentProf;
             }
