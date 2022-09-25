f79cf133eeed055f64a4fb959cc266fc124c3fef
Simran Bhatt <35162886+simranbhatt@users.noreply.github.com>
2022-09-25 19:25:35 +0530

    Time: 45 ms (11.98%), Space: 52.7 MB (15.75%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> magazineMap = new HashMap<>();
+        //adding all magazine letters to a hashmap
+        for(char letter : magazine.toCharArray())
+            magazineMap.merge(letter, 1, Integer::sum);
+        
+        //checking if all ransomNote letters are present in the magazine hashmap
+        for(char letter : ransomNote.toCharArray()) {
+            if(magazineMap.containsKey(letter) && magazineMap.get(letter) > 0)
+                magazineMap.put(letter, magazineMap.get(letter) - 1);
+            else
+                return false; 
+        }
+        return true;
+    }
+}
