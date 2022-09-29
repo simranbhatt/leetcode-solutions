88aa6118a9e8926d1b0b5505d03a3b4a6e77f6d8
Simran Bhatt <35162886+simranbhatt@users.noreply.github.com>
2022-09-29 15:52:20 +0530

    Time: 13 ms (33.27%), Space: 46.1 MB (50.86%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+
+class Solution {
+    public int romanToInt(String romanNumber) {
+        int integerValue = 0;
+        HashMap<Character, Integer> romanNumeralDictionary = new HashMap<>();
+        romanNumeralDictionary.put('I', 1);
+        romanNumeralDictionary.put('V', 5);
+        romanNumeralDictionary.put('X', 10);
+        romanNumeralDictionary.put('L', 50);
+        romanNumeralDictionary.put('C', 100);
+        romanNumeralDictionary.put('D', 500);
+        romanNumeralDictionary.put('M', 1000);
+        
+        for(int i = 0; i < romanNumber.length(); i++) {
+            int currentValue = romanNumeralDictionary.get(romanNumber.charAt(i));
+            if(i+1 < romanNumber.length()) {
+              int nextValue = romanNumeralDictionary.get(romanNumber.charAt(i+1));
+              if(currentValue < nextValue) {
+                    integerValue += nextValue - currentValue;
+                    i++;
+              } else {
+                  integerValue += currentValue;
+              }
+            }  else {
+                integerValue += currentValue;
+            }
+        }
+        return integerValue;
+    }
+      
+}
