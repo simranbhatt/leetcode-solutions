54b0ca7e6d90e96ce1637b8204e71f7921782362
Simran Bhatt <35162886+simranbhatt@users.noreply.github.com>
2022-10-02 18:54:44 +0530

    Time: 9 ms (42.23%), Space: 42.3 MB (59.87%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int longestPalindrome(String allLetters) {
+        
+        HashMap<Character, Integer> characterOccurrences = new HashMap<>();
+        int lengthOfLongestPalindrome = allLetters.length();
+        int oddOccurrences = 0;
+        boolean unpairedLetterExists = false;
+        
+        
+        for(char letter : allLetters.toCharArray()) {
+            characterOccurrences.merge(letter, 1, Integer::sum);
+        }
+    
+        for(var letter : characterOccurrences.entrySet()) {
+           int occurrence = letter.getValue();
+        
+            if(occurrence % 2 != 0) {
+                oddOccurrences++;
+            }                  
+        } 
+        
+        //one letter with odd/single occurrence(s) can exist in a palindrome
+        if(oddOccurrences >= 1)
+            oddOccurrences--;
+        
+        //any more than one letter with odd/single occurrence(s) have to be made even 
+        return lengthOfLongestPalindrome - oddOccurrences;
+    }
+}
