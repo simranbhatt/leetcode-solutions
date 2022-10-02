85eb2591a3407b728a88868c3f73b2f3b66fd064
Simran Bhatt <35162886+simranbhatt@users.noreply.github.com>
2022-10-02 19:13:18 +0530

    Time: 4 ms (64.21%), Space: 56.5 MB (28.71%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,7 +1,6 @@
 class Solution {
     public int majorityElement(int[] arrayOfNumbers) {
         Arrays.sort(arrayOfNumbers);
-        //any value that takes up more than half the array will be at the n/2 position when sorted
         return arrayOfNumbers[arrayOfNumbers.length/2];
     }
 }
