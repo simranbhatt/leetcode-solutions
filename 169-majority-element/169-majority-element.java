c099b3625211fa5028705e18e0af66843146e2c6
Simran Bhatt <35162886+simranbhatt@users.noreply.github.com>
2022-10-02 19:10:31 +0530

    Time: 2 ms (91.76%), Space: 45.8 MB (95.58%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,16 +1,6 @@
 class Solution {
     public int majorityElement(int[] arrayOfNumbers) {
-        HashMap<Integer, Integer> occurrenceMap = new HashMap<>();
-        
-        for(int number : arrayOfNumbers) {
-            occurrenceMap.merge(number, 1, Integer::sum);
-        }
-        
-        for(var number : occurrenceMap.entrySet()) {
-            if(number.getValue() > arrayOfNumbers.length/2)
-                return number.getKey();
-        }
-        
-        return 0;
+        Arrays.sort(arrayOfNumbers);
+        return arrayOfNumbers[arrayOfNumbers.length/2];
     }
 }
